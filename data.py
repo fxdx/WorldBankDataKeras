27f0a7de861316688dd7d7f90e032d59e7100a33
Paweł <pawel.dlugosz@multicom.krakow.pl>
2019-06-29 00:06:35 +0200

    excluding graph to other object

--- data.py
+++ data.py
@@ -1,6 +1,7 @@
 import pandas
 import matplotlib.pyplot as plt
 import xls_parse
+import graph_plotting
 
 class CountryInformations:
 
@@ -23,29 +24,13 @@ class CountryInformations:
                                                                   self.renewable_electricity_status)
         return print_string
 
-    def plot_graph(self, x_data, y_data, y_axis_info, title):
-        figure_plot = plt.figure()
-
-        main_graph = figure_plot.add_subplot(111)
-
-        main_graph.plot(x_data, 
-                        y_data, 
-                        color='lightcoral', 
-                        linewidth=3,
-                        linestyle='--')
-
-        main_graph.set_xlim(x_data[0], x_data[-1])
-
-        plt.title(title)
-        plt.xlabel('Year')
-        plt.ylabel(y_axis_info)
-
-        plt.show(main_graph)
-
 
 # Testing Purposes
 if __name__ == "__main__":
-    country = CountryInformations('Poland')
-    country.plot_graph(list(country.co2_emissions.keys()), 
-                       list(country.co2_emissions.values()), 'CO2 Emissions (KT)', 
-                       '{} CO2 Emissions'.format(country.name))
+    country1 = CountryInformations('Poland')
+    country2 = CountryInformations('Germany')
+    country3 = CountryInformations('France')
+
+    graph = graph_plotting.GraphPlotting(country1, country2, country3)
+    graph.plot_graph_co2_emissions()
+    
